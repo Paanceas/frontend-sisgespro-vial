42cae21e8c7e04dfcfb825790b067651882e376d
Pablo Ceballos <pablo-s_972011@hotmail.com>
2021-11-12 21:44:42 -0500

    ⬇️ version de google maps

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   apiUrl: 'https://sisgesprovial.azurewebsites.net/api/',
-  key:'AIzaSyCIDUBM6nFG-SXXeuRA3V9JAWv8ZUlKbcw',
+  key:'AIzaSyCIDUBM6nFG-SXXeuRA3V9JAWv8ZUlKbcw3EWS',
   services:{
     user:"user.php",
     vendors:"vendors.php",
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: false,
   apiUrl: 'http://localhost/api/',
-  key:'AIzaSyCIDUBM6nFG-SXXeuRA3V9JAWv8ZUlKbcw',
+  key:'AIzaSyCIDUBM6nFG-SXXeuRA3V9JAWv8ZUlKbcw3EWS',
   services:{
     user:"user.php",
     vendors:"vendors.php",
